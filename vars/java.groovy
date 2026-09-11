e43dec95e9842126f9e5cbcb8304146a2e6a8012
Pramod Kumar Patel <98573731+pp492312@users.noreply.github.com>
2026-09-11 15:10:46 +0530

    Implement Jenkins pipeline in java.groovy

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,1 +1,22 @@
-f
+def call(Map config){
+    pipeline{
+        tools{
+            maven "${config.mvnt}"
+        }
+        agent any
+        stages{
+            stage('Checkout'){
+                steps{
+                    git branch : config.branch,
+                    credentialsId : config.credid,
+                    url : config.repourl
+                }
+            }
+            stage('Build'){
+                steps{
+                    sh "mvn ${config.mvncommand}"
+                }
+            }
+        }
+    }
+}
